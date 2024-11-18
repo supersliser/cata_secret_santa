3a478b3d6c5b19d2afb8dcac87c4feeb156d5275
supersliser <thomaslower9@gmail.com>
2024-11-18 00:12:02 +0000

    safsfs

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'CATA Secret Santa',
       theme: ThemeData(
         // This is the theme of your application.
         //
