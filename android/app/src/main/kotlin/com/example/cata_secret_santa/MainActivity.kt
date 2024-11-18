d66640a67facd26f96fa7c47e7b3943a4ec3ec5f
supersliser <thomaslower9@gmail.com>
2024-11-18 00:03:04 +0000

    done

---
+++ android/app/src/main/kotlin/com/example/cata_secret_santa/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.cata_secret_santa
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,192 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final nameController = TextEditingController();
+  final promptController = TextEditingController();
+  final emailController = TextEditingController();
+
+    Future<http.Response> getUsers() async {
+  return await http.get(Uri.parse('https://api.jsonbin.io/v3/b/673a7dfcacd3cb34a8aa3089'));
+}
+
+Future<http.Response> addUser() async {
+  String name = nameController.text;
+  String prompt = promptController.text;
+  List<dynamic> users = [];
+  List<dynamic> prompts = [];
+  List<dynamic> emails = [];
+  var temp = await getUsers();
+  var data = jsonDecode(temp.body)['record'];
+  users = data['users'];
+  prompts = data['prompts'];
+  users.add(name);
+  prompts.add(prompt);
+  return http.put(
+    Uri.parse('https://api.jsonbin.io/v3/b/673a7dfcacd3cb34a8aa3089'),
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+      'X-Master-Key': r"$2a$10$8B24A50wKzRmmFYROMEYgeXIoLOQwuWEpRz6AafZLeWBAJOxrwSLC",
+      'X-Access-Key': r"$2a$10$zsqFi1oye8X3pF1UKbAUC.hb34/YEhvi2iDLbN5Xc7MNwb3rcWdOq",
+    },
+    body: jsonEncode(<String, List<dynamic>>{
+      'email': emails,
+      'users': users,
+      'prompts': prompts
+    }),
+  );
+}  
+
+Future<List<dynamic>> getSecretSanta() async {
+  int currentUser = 0;
+  List<dynamic> users = [];
+  List<dynamic> prompts = [];
+  List<dynamic> emails = [];
+  List<dynamic> usersGotSanta = [];
+  List<dynamic> usersUsedForSanta = [];
+
+    var temp = await getUsers();
+  var data = jsonDecode(temp.body)['record'];
+  users = data['users'];
+  prompts = data['prompts'];
+  emails = data['email'];
+  usersGotSanta = data['usersGotSanta'];
+  usersUsedForSanta = data['usersUsedForSanta'];
+
+  for (int i = 0; i < users.length; i++) {
+    if (emails[i] == emailController.text) {
+      currentUser = i;
+      break;
+    }
+  }
+
+  int santaUser = -1;
+
+  if (!usersGotSanta.contains(currentUser)) {
+    usersGotSanta.add(currentUser);
+    while(usersUsedForSanta.contains(santaUser) || santaUser == currentUser || santaUser == -1) {
+      santaUser = Random().nextInt(users.length);
+    }
+    usersUsedForSanta.add(santaUser);
+  }
+  else {
+    return [null, null, 0];
+  }
+
+  return [users[santaUser], prompts[santaUser], 1];
+}
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text("Welcome to BUNCCACATAL5 Secret Santa"),
+      ),
+      body: DateTime.now().month == 12 ? FutureBuilder(future: getSecretSanta(), builder: (context, snapshot) => Center(child: snapshot.data![2] == 1 ? Column(
+        children: [
+          Text("Your Secret Santa is: ${snapshot.data![0]}\n\nYour Secret Santa's Prompt is: ${snapshot.data![1]}"),
+          Text("Please make sure to take a screenshot of this as this data will not be saved"),
+          Text("*its not saved as this means that even Tom cannot access it*"),
+        ],
+      ) : Center(child: Text("You have already been assigned a Secret Santa, if you have lost this info then please hate yourself coz now we have to start the process all over again")))) : Center(
+        child: FutureBuilder(future: null, builder: (context, snapshot) => Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+                        Padding(
+              padding: const EdgeInsets.only(left: 20.0, right: 20.0, bottom: 20.0),
+              child: TextFormField(
+                controller: emailController,
+                style: TextStyle(color: Colors.white),
+                decoration: const InputDecoration(
+                  border: UnderlineInputBorder(),
+                  labelText: 'Email',
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0, right: 20.0, bottom: 20.0),
+              child: TextFormField(
+                controller: nameController,
+                style: TextStyle(color: Colors.white),
+                decoration: const InputDecoration(
+                  border: UnderlineInputBorder(),
+                  labelText: 'Name',
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0, right: 20.0, bottom: 20.0),
+              child: TextFormField(
+                controller: promptController,
+                style: TextStyle(color: Colors.white),
+                decoration: const InputDecoration(
+                  border: UnderlineInputBorder(),
+                  labelText: 'Prompt (optional)',
+                ),
+              ),
+            ),
+            ElevatedButton(onPressed: addUser, child: const Text("Submit")),
+          ],
+        ),) 
+      ),
+    );
+  }
+}
